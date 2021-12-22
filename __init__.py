76e4576ceead542776e4e9bd33f5ed5dc529e677
osagie eboigbe <osagie_eboigbe@yahoo.com>
2021-12-22 17:30:32 +0100

    add a file

---
+++ __init__.py
@@ -0,0 +1,5 @@
+
+from app.flaskapp import app
+#main driver function
+if __name__ == "__main__":
+    app.run()
---
+++ app/flaskapp.py
@@ -0,0 +1,19 @@
+from  app.simple_interest import simple_interest
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello():
+    return "This is my first API!";
+
+@app.route('/post', methods=["GET","POST"])
+def interest_score():
+     input_json = request.get_json(force=True)
+     principal= input_json["principal"]
+     rate= input_json["rate"]
+     period= input_json["period"]
+     interest=simple_interest(principal,rate,period)
+     return jsonify(interest)
